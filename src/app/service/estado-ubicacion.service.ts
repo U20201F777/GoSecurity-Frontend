53b99b9437433fb1878e6fdf01682e7bf67698f4
Kacchan368 <angelochipulina@gmail.com>
2023-10-31 07:59:38 -0500

    estadoubicacion

--- src/app/service/estado-ubicacion.service.ts
+++ src/app/service/estado-ubicacion.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { EstadoUbicacion } from '../model/estadoUbicacion';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+const base_url = environment.base
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstadoUbicacionService {
-
-  constructor() { }
+  private url = `${base_url}/EstadoUbicacion`;
+  private ListaCambio = new Subject<EstadoUbicacion[]>();
+  constructor(private http: HttpClient) { }
+  
+  Insert(estadoUbicacion: EstadoUbicacion) {
+    return this.http.post(this.url, estadoUbicacion);
+  }
+  SetList(ListaNueva: EstadoUbicacion[]) {
+    this.ListaCambio.next(ListaNueva);
+  }
+  GetList() {
+    return this.ListaCambio.asObservable();
+  }
+  Delete(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
 }
