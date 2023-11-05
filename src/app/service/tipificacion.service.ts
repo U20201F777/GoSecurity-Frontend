069cdba01ac9c5c313b7c1a88504ab10468217db
Kacchan368 <angelochipulina@gmail.com>
2023-11-04 19:32:54 -0500

    services

--- src/app/service/tipificacion.service.ts
+++ src/app/service/tipificacion.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Tipificacion } from '../model/tipificacion';
+import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+const base_url = environment.base
 
 @Injectable({
   providedIn: 'root'
 })
 export class TipificacionService {
+  private url = `${base_url}/denunciastipificacion`;
+  private ListaCambio = new Subject<Tipificacion[]>();
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  List() {
+    return this.http.get<Tipificacion[]>(this.url);
+  }
+  Insert(tipificacion: Tipificacion) {
+    return this.http.post(this.url, tipificacion);
+  }
+  SetList(ListaNueva: Tipificacion[]) {
+    this.ListaCambio.next(ListaNueva);
+  }
+  GetList() {
+    return this.ListaCambio.asObservable();
+  }
+  Delete(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+  Update(tipificacion: Tipificacion) {
+    return this.http.put(this.url, tipificacion);
+  }
 }
--- src/app/service/ubicacion-p.service.ts
+++ src/app/service/ubicacion-p.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { UbicacionP } from '../model/ubicacionP';
+import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+
+const base_url = environment.base
 
 @Injectable({
   providedIn: 'root'
 })
 export class UbicacionPService {
+  private url = `${base_url}/UbicacionP`;
+  private ListaCambio = new Subject<UbicacionP[]>();
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  List() {
+    return this.http.get<UbicacionP[]>(this.url);
+  }
+  Insert(ubicacionP: UbicacionP) {
+    return this.http.post(this.url, ubicacionP);
+  }
+  SetList(ListaNueva: UbicacionP[]) {
+    this.ListaCambio.next(ListaNueva);
+  }
+  GetList() {
+    return this.ListaCambio.asObservable();
+  }
+  Delete(id: number) {
+    return this.http.delete(`${this.url}/${id}`);
+  }
+  Update(ubicacionP: UbicacionP) {
+    return this.http.put(this.url, ubicacionP);
+  }
 }
