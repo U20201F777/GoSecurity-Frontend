6cb98b1449e87f53ab82046173430f82c4f5bb32
Arlet Aquije <128516094+U20201F777@users.noreply.github.com>
2023-11-06 22:57:08 -0500

    policia-listar

--- src/app/components/policia/policia-listar/policia-listar.component.ts
+++ src/app/components/policia/policia-listar/policia-listar.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Policia } from 'src/app/model/Policia';
+import { PoliciaService } from 'src/app/service/policia.service';
 
 @Component({
   selector: 'app-policia-listar',
@@ -6,5 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./policia-listar.component.css']
 })
 export class PoliciaListarComponent {
+  dataSource: MatTableDataSource<Policia> = new MatTableDataSource();
+  displayedColumns: string[] = [
+    'codigo',
+    'Placa',
+    'FotoRostro',
+    'FotoIdentificacion',
+    'Rango',
+    'Usuario',
+    //'accion01',
+    'accion02',
+  ];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  constructor(private pS: PoliciaService) {}
+  ngOnInit(): void {
+    this.pS.List().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.pS.GetList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+  eliminar(id: number) {
+    this.pS.Delete(id).subscribe((data) => {
+      this.pS.List().subscribe((data) => {
+        this.pS.SetList(data);
+      });
+    });
+  }
+  filter(en: any) {
+    this.dataSource.filter = en.target.value.trim();
+  }
 }
