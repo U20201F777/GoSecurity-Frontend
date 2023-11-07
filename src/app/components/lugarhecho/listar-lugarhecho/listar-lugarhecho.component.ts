b66bab97783f2f0c494f98ef52b2e4e7d3218970
Kacchan368 <angelochipulina@gmail.com>
2023-11-07 15:51:22 -0500

    arreglos

--- src/app/components/lugarhecho/listar-lugarhecho/listar-lugarhecho.component.ts
+++ src/app/components/lugarhecho/listar-lugarhecho/listar-lugarhecho.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { LugarHecho } from 'src/app/model/lugarHecho';
 
 @Component({
   selector: 'app-listar-lugarhecho',
   templateUrl: './listar-lugarhecho.component.html',
   styleUrls: ['./listar-lugarhecho.component.css']
 })
-export class ListarLugarhechoComponent {
+export class ListarLugarhechoComponent implements OnInit {
+  dataSource: MatTableDataSource<LugarHecho> = new MatTableDataSource();
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor() { }
+  ngOnInit(): void {
 
+  }
 }
