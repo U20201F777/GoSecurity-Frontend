1611d81a3fd89d7dfb97f995c549e4bbcaa6f864
Kacchan368 <angelochipulina@gmail.com>
2023-11-11 19:00:55 -0500

    denuncia - angelo

--- src/app/component/denuncias/insertar-denuncias/insertar-denuncias.component.ts
+++ src/app/component/denuncias/insertar-denuncias/insertar-denuncias.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Denuncias } from 'src/app/model/denuncias';
+import { LugarHecho } from 'src/app/model/lugarHecho';
+import { DenunciasService } from 'src/app/service/denuncias.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-insertar-denuncias',
   templateUrl: './insertar-denuncias.component.html',
   styleUrls: ['./insertar-denuncias.component.css']
 })
-export class InsertarDenunciasComponent {
+export class InsertarDenunciasComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
+  denuncia: Denuncias = new Denuncias();
+  mensaje: string = '';
+  id: number = 0;
+  edicion: boolean = false;
+  listaLugarHecho: LugarHecho[] = [];
+  idLugarHechoSeleccionado: number = 0
+  constructor(private dS: DenunciasService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute) { }
+  ngOnInit(): void {
+    this.route.params.subscribe((data: Params) => {
+      this.id = data['id'];
+    });
+    this.form = this.formBuilder.group({
+      idDenuncias: [''],
+      nameDenuncias: ['', Validators.required],
+      FechaDenunciasHechos: ['', Validators.required],
+      FechaDenunciasRegistro: ['', Validators.required],
+      FechaDenunciasEmision: ['', Validators.required],
+      LugarHecho: ['', Validators.required]
+    });
+  }
+  aceptar(): void {
+    if (this.form.valid) {
+      this.denuncia.idDenuncias = this.form.value.idDenuncias;
+      this.denuncia.nameDenuncias = this.form.value.nameDenuncias;
+      this.denuncia.FechaDenunciasHechos = this.form.value.FechaDenunciasHechos;
+      this.denuncia.FechaDenunciasRegistro = this.form.value.FechaDenunciasRegistro;
+      this.denuncia.FechaDenunciasEmision = this.form.value.FechaDenunciasEmision;
+      this.denuncia.idLugarHecho.idDenunciasLugarHecho = this.form.value.LugarHecho;
 
-}
+      if (this.edicion) {
+        this.dS.Update(this.denuncia).subscribe(() => {
+          this.dS.List().subscribe((data) => {
+            this.dS.SetList(data);
+          });
+        });
+      } else {
+        this.dS.Insert(this.denuncia).subscribe((data) => {
+          this.dS.List().subscribe((data) => {
+            this.dS.SetList(data);
+          });
+        });
+      }
+      this.router.navigate(['Denuncia']);
+    } else {
+      this.mensaje = 'Por favor complete todos los campos obligatorios.';
+    }
+  }
+
+  obtenerControlCampo(nombreCampo: string): AbstractControl {
+    const control = this.form.get(nombreCampo);
+    if (!control) {
+      throw new Error(`Control no encontrado para el campo ${nombreCampo}`);
+    }
+    return control;
+  }
+ }
--- src/app/component/denuncias/listar-denuncias/listar-denuncias.component.ts
+++ src/app/component/denuncias/listar-denuncias/listar-denuncias.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Denuncias } from 'src/app/model/denuncias';
+import { DenunciasService } from 'src/app/service/denuncias.service';
 
 @Component({
   selector: 'app-listar-denuncias',
   templateUrl: './listar-denuncias.component.html',
   styleUrls: ['./listar-denuncias.component.css']
 })
-export class ListarDenunciasComponent {
-
+export class ListarDenunciasComponent implements OnInit {
+  dataSource: MatTableDataSource<Denuncias> = new MatTableDataSource();
+  displayedColumns: string[] = [
+    'idDenuncias',
+    'nameDenuncias',
+    'FechaDenunciasHechos',
+    'FechaDenunciasRegistro',
+    'FechaDenunciasEmision',
+    'LugarHecho',
+    'accion01',
+    'accion02'
+  ];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  constructor(private dS: DenunciasService) { }
+  ngOnInit(): void {
+    this.dS.List().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+    this.dS.GetList().subscribe((data) => {
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    });
+  }
+  eliminar(id: number) {
+    this.dS.Delete(id).subscribe((data) => {
+      this.dS.List().subscribe((data) => {
+        this.dS.SetList(data);
+      });
+    });
+  }
+  filter(en: any) {
+    this.dataSource.filter = en.target.value.trim();
+  }
 }
